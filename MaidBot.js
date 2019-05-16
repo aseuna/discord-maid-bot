9ecdad2f58f578a00facbb56d4b4a4a3a337d8e8
aseuna <arttuseuna@gmail.com>
2019-05-16 22:45:29 +0300

    bot source code

---
+++ MaidBot.js
@@ -0,0 +1,54 @@
+//MaidBot
+//Cleans a text channel with !cleanse
+
+// Import the discord.js module
+const Discord = require('discord.js');
+const config = require('./config.json');
+
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.once('ready', () => {
+	console.log('I am ready!');
+});
+
+client.on('message', msg => {
+	
+	
+	let deleteStuff = () => {
+		
+		
+		msg.channel.fetchMessages({limit: 100})
+		.then(messages => {
+			//creates an array of messages on the current channel
+			let messagesArr = messages.array();
+			//number of messages
+			let messageCount = messagesArr.length;
+			//deleting messages in the array
+			for(let i = messageCount - 1; i > -1; i--) {
+				messagesArr[i].delete();
+			}
+		})
+		.catch(function(err) {
+			console.log('error thrown');
+			console.log(err);
+		});
+	};
+  
+	//cleans the current channel's messages when user writes !cleanse on that particular channel
+	if(msg.content === '!cleanse') {
+		deleteStuff();
+	}
+	
+  
+});
+
+
+// login to Discord with your app's token
+// replace config.token with your own, or put your token in a config.json file
+client.login(config.token);
