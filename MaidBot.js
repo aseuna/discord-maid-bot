a86e7c7a2b7de3a2bc10dd9eaad9485f6ea7f761
aseuna <arttuseuna@gmail.com>
2019-05-18 02:56:00 +0300

    preparation for future versions
    Implemented a few variables to be used later with new features allowing discord user to manipulate bot behaviour with commands

--- MaidBot.js
+++ MaidBot.js
@@ -1,13 +1,14 @@
 //MaidBot
-//Cleans a text channel with !cleanse
+//Cleans a text channel with !clean
 
 // Imports the discord.js module
 const Discord = require('discord.js');
 //Imports node-schedule module that handles timers and scheduling
 const schedule = require('node-schedule');
-//config.json file in the same root directory as maidbot.js file
-const config = require('./config.json');
-
+//imports dotenv module to support environment variables
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
@@ -21,8 +22,25 @@ client.once('ready', function(){
 	console.log('I am ready!');
 });
 
+
+//these variables will be changeable later through user commands inside discord client
+let cleanmsg = 'Channel cleaned :sunglasses:';
+let timedchannel = 'bot-cleanup-channel';
+
+//default values for cronjob scheduling -- 0 9 * * * => the task is scheduled every day at 9 am
+//later changeable with user commands to a desired schedule
+let cronhour = 9;
+let cronmin = 0;
+//function for returning the schedule in cronjob format used with the schedule timer
+function cronschedule(cronmin, cronhour){
+	return cronmin + ' ' + cronhour + ' * * *'
+}
+
 //deletes messages on the channel received as an argument
 function deleteMessages(channel){
+	
+	//implement permission check for deleting messages later
+	//if no permission, return a message saying no permission to clean the channel
 	
 	channel.fetchMessages({limit: 100})
 	.then(function(messages){
@@ -36,8 +54,7 @@ function deleteMessages(channel){
 		}
 	})
 	.then(function(){
-		//cleanmsg string in a separate config.json file
-		channel.send(config.cleanmsg);
+		channel.send(cleanmsg);
 	})
 	.catch(function(err){
 		console.log('error thrown');
@@ -47,39 +64,38 @@ function deleteMessages(channel){
 
 
 client.on('message', function(userMsg){
-  
+	
 	let channelArr = client.channels.array();
 	//cleans a channel's messages when user writes !cleanse and the channel name
-	if(userMsg.content.startsWith('!cleanse')){
+	if(userMsg.content.startsWith('!clean')){
 		//if user message only contains !cleanse, the current channel is cleansed
-		if(userMsg.content === '!cleanse'){
+		if(userMsg.content === '!clean'){
 			deleteMessages(userMsg.channel);
 		}
 		else{
-			//searches for a channel with the name that is inside !cleanse message
+			//searches for a channel with the name that is inside !clean message
 			for(let i = 0; i < channelArr.length; i++){
 				if(userMsg.content.indexOf(channelArr[i].name) > -1){
-					deleteMessages(channelArr[i])
+					deleteMessages(channelArr[i]);
 				}
 			}
 		}
 	}
 	else if(userMsg.content === '!botinfo'){
 		//gives info about what the bot does, when user sends '!botinfo'
-		userMsg.channel.send('"!cleanse": cleans the current channel of up to 100 messages\n\n' + 
-						'"!cleanse channel-name": cleans channel-name of up to 100 messages\n\n' + 
-						'Currently has scheduled cleanup on channel "' + config.timedchannel + '"'
-						);
+		userMsg.channel.send('"!clean": cleans the current channel of up to 100 messages\n\n' + 
+					'"!clean channel-name": cleans channel-name of up to 100 messages\n\n' + 
+					'Currently has scheduled cleanup on channel "' + timedchannel + '"'
+					);
 	}
 	
 });
 
-//schedules the bot to clean up channel specified in the config.json
 //timer uses cronjob notation for scheduleJob, for example '30 18 * * *'
-let timer = schedule.scheduleJob(config.cronschedule, function(){
-	deleteMessages(client.channels.find('name', config.timedchannel));
+let timer = schedule.scheduleJob(cronschedule(cronmin, cronhour), function(){
+	deleteMessages(client.channels.find('name', timedchannel));
 });
 
 // login to Discord with your app's token
-// replace config.token with your own auth token, or put your token in a config.json file
-client.login(config.token);
+// replace process.env.TOKEN with your own auth token, or put your token in a .env file
+client.login(process.env.TOKEN);
