8ee31bdfccecf0ce17d30a03f3f700e2741026d9
aseuna <arttuseuna@gmail.com>
2019-05-19 14:55:55 +0300

    Update MaidBot.js
    implemented feature to set the message deletion limit for each cleanup, handles invalid inputs by user

--- MaidBot.js
+++ MaidBot.js
@@ -26,19 +26,23 @@ client.once('ready', function(){
 // these variables will be changeable later through user commands inside discord client
 let cleanmsg = 'Channel cleaned :sunglasses:';
 let timedchannel = undefined;
-let cleanLimit = 100;
+let timedlimit = 100;
+
+// default message deletion limit, used when user gives no limit parameter
+const DEF_LIMIT = 100;
 
 // default values for cronjob scheduling -- for example 0 0 9 * * * => the task is scheduled every day at 9 am
 // changeable with user commands to a desired schedule
 let cronhour = undefined;
 let cronmin = undefined;
 let cronsec = undefined;
-// function returns the schedule in cronjob format used with the schedule timer more about cronjob here: https://en.wikipedia.org/wiki/Cron
+// function returns the schedule in cronjob format used with the schedule timer, more about cronjob here: https://en.wikipedia.org/wiki/Cron
 function cronschedule(cronsec, cronmin, cronhour){
 	return cronsec + ' ' + cronmin + ' ' + cronhour + ' * * *'
 }
 
 // timer uses cronjob notation for scheduleJob, for example '30 18 * * *'
+// timer is by default null, until user sets the time by !settime
 let timer = null;
 
 /**
@@ -65,9 +69,9 @@ function deleteMessagesInd(channel){
 	});
 }*/
 
-// bulk deletes messages on the channel received as an argument
+// bulk deletes messages on the channel received as an argument, up to the limit
 // deletes messages up to 100 that are newer than 2 weeks -> bulkDelete feature
-function bulkDeleteMessages(channel){
+function bulkDeleteMessages(channel, limit){
 	
 	/** 
 	* implement permission check for deleting messages later
@@ -75,7 +79,7 @@ function bulkDeleteMessages(channel){
 	*/
 	// deletes the messages on the channel if found
 	if(channel != null){
-		channel.bulkDelete(cleanLimit);
+		channel.bulkDelete(limit);
 		
 		// only sends the cleaning message if it's not null
 		if(cleanmsg !== null){
@@ -91,10 +95,78 @@ client.on('message', function(userMsg){
 	
 	if(userMsg.content.startsWith('!clean')){
 		// if user message only contains !clean, the current channel is cleansed
+		
+		// sets channels in an array
+		let channelArr = client.channels.array();
+		// splits user message into an array so each part can be handled separately
+		let userMsgArr = userMsg.content.split(' ');
 		if(userMsg.content === '!clean'){
-			bulkDeleteMessages(userMsg.channel);
+			// if there are no cleaning parameters, cleaning current channel with default limit
+			bulkDeleteMessages(userMsg.channel, DEF_LIMIT);
+		}
+		else if(userMsgArr.length === 2){
+			// checks if the second part of the userMsg is a number or not
+			if(isNaN(parseInt(userMsgArr[1]))){
+				// if the second element is not a number, should be channel name
+				// searches for a channel with the name that is inside !clean message
+				let channelFound = false;	// tells if a channel has found that corresponds to user input
+				for(let i = 0; i < channelArr.length; i++){
+					if(userMsg.content.indexOf(channelArr[i].name) > -1){
+						// cleans the messages in the user specified channel if found
+						bulkDeleteMessages(channelArr[i], DEF_LIMIT);
+						channelFound = true;
+					}
+				}
+				// no channel exists with the name that is inside !clean message
+				if(!channelFound){
+					userMsg.channel.send('Invalid channel input');
+				}
+			}
+			else{
+				// if the second element is a number, it should be message deletion limit between 0-100
+				if(parseInt(userMsgArr[1]) >= 0 && parseInt(userMsgArr[1]) <= 100){
+					// deletes messages on the current channel up to the "parseInt(userMsgArr[1])" -limit
+					bulkDeleteMessages(userMsg.channel, parseInt(userMsgArr[1]));
+				}
+				else{
+					// informs user that the message deletion limit is invalid
+					userMsg.channel.send('Invalid message limit input, must be between 0-100');
+				}
+			}
+			
+		}
+		else if(userMsgArr.length === 3){
+			// the user command !clear has two parameters
+			// the second parameter should be limit for message deletion
+			if(!isNaN(parseInt(userMsgArr[2])) && parseInt(userMsgArr[2]) >= 0 && parseInt(userMsgArr[2]) <= 100){
+				
+				// searches for a channel with the name that is inside !clean message
+				let channelFound = false;	// tells if a channel has found that corresponds to user input
+				for(let i = 0; i < channelArr.length; i++){
+					if(userMsg.content.indexOf(channelArr[i].name) > -1){
+						// cleans the messages in the user specified channel if found
+						bulkDeleteMessages(channelArr[i], parseInt(userMsgArr[2]));
+						channelFound = true;
+					}
+				}
+				// no channel exists with the name that is inside !clean message
+				if(!channelFound){
+					userMsg.channel.send('Invalid channel input');
+				}
+				
+			}
+			else{
+				// informs user that the input is invalid
+				userMsg.channel.send('Invalid user input, should be "!clean channel-name limit"');
+			}
 		}
 		else{
+			// the !clean message has too many parameters 
+			// informs user that the input is invalid
+			userMsg.channel.send('Invalid user input, should be "!clean channel-name limit"');
+		}
+		
+		/*else{
 			// cleans a channel's messages when user writes !clean and the channel name
 			// sets channels in an array
 			let channelArr = client.channels.array();
@@ -105,18 +177,20 @@ client.on('message', function(userMsg){
 					bulkDeleteMessages(channelArr[i]);
 				}
 			}
-		}
+		}*/
 	}
 	else if(userMsg.content === '!botinfo'){
 		// gives info about what the bot does, when user sends '!botinfo'
-		userMsg.channel.send('"!clean": cleans the current channel of up to 100 and 2 weeks old messages\n\n' + 
-					'"!clean channel-name": cleans channel-name of up to 100 and 2 weeks old messages\n\n' + 
+		userMsg.channel.send('"!clean limit": cleans the current channel of up to 100 and 2 weeks old messages, limit(optional) is number of deleted messages 0-100\n\n' + 
+					'"!clean channel-name limit": cleans channel-name of up to 100 and 2 weeks old messages, limit(optional) is number of deleted messages 0-100\n\n' + 
 					'"!settime hrs.mins.secs": sets time for daily cleanup for a specified channel, use "." as a separator or the time is invalid, notation follows 24-hour clock, mins and secs optional\n\n' + 
 					'"!setchannel channel-name": sets the channel for daily cleanup\n\n' + 
+					'"!settimedlimit limit": sets the message deletion limit for timed clean up, limit is number of deleted messages 0-100\n\n' +
 					'"!setcleanmsg message": sets message that the bot posts after cleaning a channel\n\n' + 
-					'"!delcleanmsg": deletes the message the bot posts after cleaning a channel, no message will be posted\n\n' +
-					'Currently has scheduled cleanup on channel "' + timedchannel + '"' + ' at ' + cronhour + '.' + cronmin + '.' + cronsec + '\n\n' +
-					'Bot doesn\'t currently use datadase for storing information so clean up channel and time has to be set every time bot goes offline and online again\n\n' + 
+					'"!delcleanmsg": deletes the message the bot posts after cleaning a channel, no message will be posted\n\n' + 
+					'Default message deletion limit is 100 for every command\n\n' + 
+					'Currently has scheduled cleanup on channel "' + timedchannel + '"' + ' at ' + cronhour + '.' + cronmin + '.' + cronsec + ', messages to be deleted: ' + timedlimit +'\n\n' +
+					'Bot doesn\'t currently use datadase for storing information so data has to be set every time bot goes offline and online again\n\n' + 
 					'Remember to give the bot the required permissions to delete/manage messages on the channels you want them deleted'
  					);
 	}
@@ -155,7 +229,7 @@ client.on('message', function(userMsg){
 			}
 			// timer uses cronjob notation for scheduleJob
 			timer = schedule.scheduleJob(cronschedule(cronsec, cronmin, cronhour), function(){
-				bulkDeleteMessages(client.channels.find(ch => ch.name === timedchannel));
+				bulkDeleteMessages(client.channels.find(ch => ch.name === timedchannel), timedlimit);
 			});
 			
 			userMsg.channel.send('Clean time is now set :sunglasses:');
@@ -192,6 +266,24 @@ client.on('message', function(userMsg){
 		// sets clean up message to null
 		// no message is posted
 		cleanmsg = null;
+	}
+	else if(userMsg.content.startsWith('!settimedlimit')){
+		
+		// splits user message into an array so each part can be handled separately
+		let userMsgArr = userMsg.content.split(' ');
+		
+		// the second element in the user message should be the message limit parameter, a value between 0-100
+		if(!isNaN(parseInt(userMsgArr[1])) && parseInt(userMsgArr[1]) >= 0 && parseInt(userMsgArr[1]) <= 100 && userMsgArr.length === 2){
+			
+			// sets the timed limit to the value in the user command parameter
+			timedlimit = parseInt(userMsgArr[1]);
+			userMsg.channel.send('Limit set :sunglasses:');
+		}
+		else{
+			// informs user that the input is invalid
+			userMsg.channel.send('Invalid user input, should be "!settimedlimit limit" limit must be 0-100');
+		}
+		
 	}
 	
 	// user input options end here
